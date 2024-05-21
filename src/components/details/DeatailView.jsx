a952c2e95933a381e7ae2d70dc568e60eb412840
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-05-22 01:02:47 +0530

    Updated Poem pic

--- src/components/details/DeatailView.jsx
+++ src/components/details/DeatailView.jsx
@@ -76,7 +76,7 @@ const DetailView = () => {
       setPic("https://img.freepik.com/premium-photo/social-media-blog-concept-futuristic-icon-design-graphics-hand-with-smartphone_102583-6104.jpg");
     }
     if(Post.category === "Poem"){
-      setPic("https://img.freepik.com/free-vector/hand-drawn-fashion-shop-pattern-background_23-2150849915.jpg");
+      setPic("https://c0.wallpaperflare.com/preview/181/626/366/feather-filler-poetry-poetic.jpg");
     }
     if(Post.category === "General"){
       setPic("https://images.unsplash.com/opengraph/1x1.png?blend=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1432821596592-e2c18b78144f%3Fblend%3D000000%26blend-alpha%3D10%26blend-mode%3Dnormal%26crop%3Dfaces%252Cedges%26h%3D630%26mark%3Dhttps%253A%252F%252Fimages.unsplash.com%252Fopengraph%252Fsearch-input.png%253Fh%253D84%2526txt%253Dblog%2526txt-align%253Dmiddle%25252Cleft%2526txt-clip%253Dellipsis%2526txt-color%253D000000%2526txt-pad%253D80%2526txt-size%253D40%2526txt-width%253D660%2526w%253D750%2526auto%253Dformat%2526fit%253Dcrop%2526q%253D60%26mark-align%3Dmiddle%252Ccenter%26mark-w%3D750%26w%3D1200%26auto%3Dformat%26fit%3Dcrop%26q%3D60%26ixid%3DM3wxMjA3fDB8MXxzZWFyY2h8NHx8YmxvZ3xlbnwwfHx8fDE3MTU1NzY0ODJ8MA%26ixlib%3Drb-4.0.3&blend-w=1&h=630&mark=https%3A%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&mark-align=top%2Cleft&mark-pad=50&mark-w=64&w=1200&auto=format&fit=crop&q=60");
